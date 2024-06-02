db41d3ad429e4563250378ecbe085aa0fd9a3a99
nelson rodriguez <nelsonrodriguez@hotmail.cl>
2024-06-02 19:53:55 -0400

    seccion2: terminada

--- 01-Fundamentals-Part-1/starter/Desafios.js
+++ 01-Fundamentals-Part-1/starter/Desafios.js
@@ -1,4 +1,5 @@
-console.log('Desafio 1');
+console.log('DESAFIO 1');
+/*
 //Mark y John están intentando comparar su IMC (índice de masa corporal), que se calcula mediante la fórmula: BMI = mass / height ** 2 = mass / (height * height)(masa en kg y altura en metros).
 
 //Tu tarea es escribir un código para ayudarlos:
@@ -13,11 +14,84 @@ const massJohn = 92;
 const heightJohn = 1.95;
 
 //2- Calcule el IMC de ambos usando la fórmula y almacene los resultados en dos variables llamadas BMIMarky BMIJohn.
-const bmiMark = (massMark / heightMark) * heightMark;
-const bmiJohn = (massJohn / heightJohn) * heightJohn;
+const bmiMark = massMark / (heightMark * heightMark);
+const bmiJohn = massJohn / (heightJohn * heightJohn);
 //3- Registre el valor de BMIMarky BMIJohnen la consola.
 console.log(bmiMark, bmiJohn);
 //4- BONIFICACIÓN : cree una variable booleana markHigherBMI que contenga información sobre si Mark tiene un IMC más alto que John. Regístrelo en la consola también
 
 const markHigherBMI = bmiMark > bmiJohn;
 console.log(markHigherBMI);
+*/
+console.log('DESAFIO2 2');
+/*
+//Utilice el ejemplo de BMI del Desafío n.º 1 y el código que ya escribió y mejórelo:
+
+//1. Imprima un buen resultado en la consola, indicando al usuario quién tiene el IMC más alto. El mensaje puede ser:
+//"Mark's BMI is higher than John's!"o "John's BMI is higher than Mark's!".
+if (bmiMark > bmiJohn) {
+  console.log(`el IMC de mark es mas alto que el de john`);
+} else {
+  console.log(`el IMC de John es mas alto que el de mark`);
+}
+
+//2. Modifique los resultados anteriores para utilizar literales de plantilla para incluir los valores de IMC en los resultados.
+//Ejemplo: "Mark's BMI (28.3) is higher than John's (23.9)!"o "John's BMI (29.1) is higher than Mark's (27)!".
+if (bmiMark > bmiJohn) {
+  console.log(`el IMC de mark (${bmiMark}) es mas alto que el de john (${bmiJohn})`);
+} else {
+  console.log(`el IMC de john (${bmiJohn}) es mas alto que el de mark (${bmiMark})`);
+}
+*/
+console.log('DESAFIO 3');
+/*
+//Hay dos equipos de gimnasia: Delfines y Koalas. Compiten entre sí 3 veces. ¡El ganador con el puntaje promedio más alto gana un trofeo!
+
+//DATOS DE PRUEBA: Los delfines obtuvieron 96, 108 y 89. Los koalas obtuvieron 88, 91 y 110.
+
+//1. Calcule el puntaje promedio de cada equipo, utilizando los datos de las pruebas que se incluyen a continuación. Se debe asignar la puntuación promedio de los delfines a la scoreDolphins variable y la puntuación promedio de los koalas se debe asignar a la scoreKoalas variable.
+const scoreDolphins = (97 + 112 + 101) / 3;
+const scoreKoalas = (109 + 95 + 106) / 3;
+console.log(scoreDolphins, scoreKoalas);
+
+//2. Compare las puntuaciones promedio del equipo para determinar el ganador de la competencia e imprima en la consola:
+if (scoreDolphins > scoreKoalas) {
+  console.log('Dolphins win the trophy');
+} else if (scoreKoalas > scoreDolphins) {
+  console.log('Koalas win the trophy');
+} else {
+  console.log('Both win the trophy');
+}
+//3. Bonificación 1: Incluir el requisito de una puntuación mínima de 100. Con esta regla, El equipo solo gana si tiene una puntuación más alta que el otro equipo, y al mismo tiempo una  puntuación de al menos 100 puntos. Sugerencia: use un operador lógico para probar el mínimo puntuación, así como múltiples bloques else-if 😉
+
+//Bonus 2:Bonificación 2: ¡la puntuación mínima también se aplica en caso de empate! Entonces un empate sólo ocurre cuando ambos equipos tienen el mismo puntaje y ambos tienen un puntaje mayor o igual a 100 puntos. De lo contrario, ningún equipo ganará el trofeo.
+if ((scoreDolphins > scoreKoalas) & (scoreDolphins >= 100)) {
+  console.log('los desfines son  los ganadores');
+} else if ((scoreKoalas > scoreDolphins) & (scoreKoalas >= 100)) {
+  console.log('los koalas son los ganadores');
+} else if ((scoreDolphins === scoreKoalas) & (scoreDolphins >= 100) & (scoreKoalas >= 100)) {
+  console.log('empate de ambos equipos');
+} else {
+  console.log('no hay equipos ganadores');
+}
+
+// Data Bonus 1: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 123
+//Data Bonus 2: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 106
+*/
+console.log('DESAFIO 4');
+//Steven quiere construir una calculadora de propinas muy simple para cada vez que va a comer a un restaurante. En su país es habitual dejar una propina del 15% si el valor de la factura está entre 50 y 300. Si el valor es diferente, la propina es del 20%.
+
+//Datos de prueba:
+//§ Dato 1: Prueba para billetes con valores 275, 40 y 430
+//Consejos:
+//§ Para calcular el 20% de un valor, simplemente multiplíquelo por 20/100 = 0,2
+//§ El valor X está entre 50 y 300, si es >= 50 && <= 300
+
+//1. Calcule la propina, según el valor de la factura. Cree una variable llamada 'tip' para este. No está permitido usar una declaración if/else 😅 (Si te resulta más fácil, puedes comience con una declaración if/else y luego intente convertirla a un ternario ¡operador!)
+const factura = 430;
+const propina = factura >= 50 && factura <= 300 ? 275 * (15 / 100) : factura * (20 / 100);
+
+//2. Imprime una cadena en la consola que contiene el valor de la factura, la propina y el valor final. (factura + propina). Ejemplo: “La factura fue 275, la propina fue 41,25 y el valor total 316,25”
+console.log(
+  `la factura salio ${factura} la propina salio ${propina} en total es ${factura + propina}`
+);
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -135,6 +135,7 @@ console.log(añoActual - 1990 > añoActual - 1018);
 
 console.log('precedencia de operadores');
 
+/*
 //¿como sabe javascript que operador debe ejecutar primero y cual no?.
 //esto es gracias a un par de reglas definidas conocidas como precedencia de operadores
 const añoActual = 3000;
@@ -156,3 +157,266 @@ console.log(x, y);
 //por ultimo veamos el operador de mayor precedencia ()
 const averageAge = (edadJonas + edadMonica) / 2;
 console.log(edadJonas, edadMonica, averageAge);
+*/
+console.log('plantillas literales');
+/*
+
+//de la forma tradicional
+const firtName = 'nelson';
+const job = 'teacher';
+const birthYear = 1991;
+const year = 2037;
+
+const nelson = ' soy ' + firtName + ' un ' + job + ' de ' + (year - birthYear) + ' años ' + '☝️';
+console.log(nelson);
+
+//utilizandom una plantilla literal
+const nuevoNelson = ` soy ${firtName} un ${job} de ${year - birthYear} años`;
+console.log(nuevoNelson);
+
+//otra cosa que viene a solucionar es cuando necesitamos saltos de linea
+console.log(
+  'esta es una \n\
+    oracion con \n\
+    saltos de linea'
+);
+
+//pero ahora con las plantillas es tan facil como hacer enter
+console.log(`esta es una
+oracion con
+saltos de linea`);
+*/
+
+console.log('tomando desiciones');
+
+/*
+const age = 18;
+const esApta = age >= 18;
+
+//esta es una estructura de control
+if (esApta) {
+  console.log('es apta para conducir 😊');
+} else {
+  console.log('no es apta para conducir 😢');
+}
+
+//veamos otro ejemplo, para saber si la persona nacio en el siglo 20 o en el siglo 21
+const birthYear = 2012;
+//la variable century debemos declararla fuera ya que si no lo hacemos esta no sera leida DESPUES
+let century;
+if (birthYear <= 2000) {
+  century = 20;
+} else {
+  century = 21;
+}
+console.log(century);
+*/
+console.log('conversion y coersion de datos');
+/*
+//CONVERSION (le indicamos explicitamente que haga el cambio)
+const inputYear = '1991';
+//tener en cuenta que esta funcion 'Number' solo nos convertira nuestro string en donde esta este presente, en otras palabras la variable original sigue y seguira siendo string.
+console.log(Number(inputYear), inputYear);
+console.log(Number(inputYear) + 18);
+console.log(typeof inputYear);
+
+//a menos que le digamos explicitamente(conversion) que queremos cambiarla. y para eso devemos hacerlo en el lugar en donde se declara
+
+const inputYear1 = Number('1991');
+// observamos que ya no es necesario declararla en todas partes, solo dende esta de define y ya
+console.log(inputYear1, inputYear1);
+console.log(inputYear1 + 18);
+console.log(typeof inputYear1);
+
+//hay ocasiones en que jv no podra convertir a un numero aunque se lo indiquemos explicitamente, en este caso nelson no puede ser un numero por lo  que nos arrojara NAN
+console.log(Number('nelson'));
+
+//pero si lo hacemos al revez si funcionara, (un texto no puede ser un numero, pero un numero si puede ser un string)
+console.log(String(23), 23);
+
+//COERSION(es jv el que hara el cambio automaticamente y detras de escena)
+console.log('I am ' + 23 + ' years old');
+console.log('23' - '10' - 3);
+//*siempre jv intentara cambiar el tipo de dato de uno o varios datos basicamente para que la operacion que se intenta hacer se haga
+console.log('23' * '23');
+
+//veamos otros ejemplos para analizar el comportamiento de jv
+let n = '1' + 1; //como el operador es (+) convertira el numero a string por lo que obtendrmos un (11 string)
+//como el operador es (-) sera el string es que sera convertido a numero
+n = n - 1;
+console.log(n); //por lo que es resultado de n sera 10.
+
+//ultimo ejemplo
+console.log(2 + 2 + 2 + 2 + '2'); //obtenfremos un 8 string el que se concatenara con un 2 string
+*/
+
+console.log('valores de falsedad y verdad');
+/*
+//tenemos 5 valores que no son considerados booleanos hasta que los usamos, estos valores son
+//=> 0, undefined, ' ', null, NaN
+
+//este ejemplo es solo una demostracion ya que en la practica la transformacion es inplicita(COERSION)
+console.log(Boolean(0)); // false
+console.log(Boolean(undefined)); //false
+console.log(Boolean('jonas')); //true
+console.log(Boolean({})); //true
+console.log(Boolean(' ')); //false
+console.log(Boolean(null)); //false
+console.log(Boolean(NaN)); //false
+
+//veamos un ejemopllo practico a la hora de usar estos valores, IMPORTANTE devemos saber que en un bloque de codigo de una dondicional como es este caso, jv siempre intentara convertir la condicion a un booleano, como nuestro valor es 0 y ya sabemos que este valor se transformara a un valor false. es por esto el resultado que nos da.
+const money = 0;
+if (money) {
+  console.log('hay dinero');
+} else {
+  console.log('no hay dinero');
+}
+
+//veamos otro ejemplo a tener en cuenta, como la variable peso no esta definida esta se convertira en un valor false, por lo tanto el resultado que nos da
+let peso;
+if (peso) {
+  console.log('buen peso choro');
+} else {
+  console.log('el peso es indefinido');
+}
+
+//el conportamiento que hemos visto de estos valores podria traer errores esto lo veremos DESPUES
+*/
+console.log('Equality Operators');
+
+/*
+//NOTA se recomienda usar siempre el operador de igualdad estricto y realizar los cambios de tipo de dato manualmente
+const age = '18';
+//este operador stricto NO realiza coersion, significa que no cambiara el tipo de dato
+if (age === 18) console.log('you just became an adult (Strict)');
+//este operador SI realiza corsion significa que convertira el '18' y lo cambiara a number
+if (age == 18) console.log('you just becamme an adult (loose)');
+
+//pasemos ahora a como obtener un valor desde una pagina web
+const favorite = prompt("what' s your favorite number");
+console.log(favorite);
+console.log(typeof favorite);
+
+//*notar el valor que estamos pidiendo es de tipo number ,pero debemos saber que la funcion PROMT simpre nos devolvera cadenas(string) y si usamos el operador de igualdad que no es estricto commo en este ejemplo nos arrojara el resultado como si estuviera bien
+if (favorite == 23) {
+  console.log('esta operacion parece correcta, pero no lo es');
+}
+
+//es por casos como este que es sencillo que se recomienda usar siempre el estricto y realizar el cambio de tipo manualmente
+const favorite1 = Number(prompt("what' s your favorite number"));
+
+//tambien podemos seguir preguntando otras condiciones
+if (favorite1 === 23) {
+  console.log('cool ! este numero 23 si es el real y es amazing');
+} else if (favorite === 7) {
+  console.log('cool ! este numero 7 si es el real y es amazing');
+} else if (favorite === 9) {
+  console.log('cool ! este numero 9 si es el real y es amazing');
+} else {
+  console.log('el numero ingresado no es 23, 7 o 9');
+}
+
+//por ultimo veamos un ejemplo de del operador de DESIGUALDAD ya que hasta ahora solo hemos visto operadores de igualdad, //¿pero y si queremos comparar, prefuntar si son diferentes?
+if (favorite1 !== 23) console.log('correcto es diferente');
+else {
+  console.log('no, el numero inggresado es igual a 23');
+}
+*/
+
+console.log('Logica Bolleana');
+/*
+
+const tieneLicencia = true;
+const tieneBuenaVision = true;
+
+//todas deven ser true para que el resultado final sea true de lo contrario sera false
+console.log(tieneLicencia && tieneBuenaVision);
+//todas deben ser false para que el resultado final sea false de lo contrario sera true
+console.log(tieneLicencia || tieneBuenaVision);
+//invertira el valor el valor
+console.log(!tieneLicencia);
+
+//ejemplo usando el bloque if
+if (tieneLicencia && tieneBuenaVision) {
+  console.log('nelson is a able to driver');
+} else {
+  console.log('someone else shold  drive');
+}
+
+//agregamos una tercera variable
+const estaCansada = false;
+console.log(tieneLicencia && tieneBuenaVision && estaCansada);
+
+//ahora con esta tercera variable mejoremos nuestra condicion, como la variable (estaCansada) esta en true podemos usar el operador de negacion para invertir y que nos arroja que si puede manejar
+if (tieneLicencia && tieneBuenaVision && !estaCansada) {
+  console.log('nelson is a able to driver');
+} else {
+  console.log('someone else shold  drive');
+}
+*/
+console.log('The switch Statement-->DECLARACION DE CAMBIO');
+/*
+//cuando tenemos una sentencia if que tiene muchas opciones, tenemos la opcion de usar o reemplazarla por esta declaracion (switch), anque esta se utiliza poco aun la podremos encontrar por hay.
+//*debemos saber que esta declaracion compara de manera ESTRICTA.
+//*los breack son importantes ya que sin ellos todas las lineas de codigo se ejecutarian, en otras palabras no realizaria la comparacion y simplemente ejecutara todo.
+
+const day = 'wednesday';
+switch (day) {
+  case 'monday':
+    console.log('planificar la esctructura del curso');
+    break;
+  case 'tuesday':
+    console.log('preparar los videos');
+    break;
+  //podemos especificar 2 o mas valores para comparar
+  case 'wednesday':
+  case 'thursday':
+    console.log('codificar ejemplos');
+    break;
+  case 'friday':
+    console.log('editar videos');
+    break;
+  case 'saturday':
+  case 'sunday':
+    console.log('descansar');
+    break;
+  default:
+    console.log('dia ingresado incorrecto');
+}
+
+//realizamos el mismo ejercicio con la declaracion de if
+if (day === 'monday') {
+  console.log('planificar la estructura de la clase');
+} else if (day === 'tuesday') {
+  console.log('preparar los videos');
+} else if (day === 'wednesday' || day === 'thursday') {
+  console.log('codificar ejemplos');
+} else if (day === friday) {
+  console.log('editar videos');
+} else if (day === 'saturday' || day === 'sanday') {
+  console.log('descansar');
+} else {
+  console.log('el dia es invalido');
+}
+*/
+console.log('OPERADOR TERNARIO');
+
+//este operador, nos servira en ocasiones para reemplazar la declaracion if, pero debemos saber que no es un reemplazo ya que este operador solo permite 1 comparacion para operaciones mas grandes siempre deberemos usar if o switch
+const age = 23;
+age >= 18 ? console.log('puedo beber vino🍷') : console.log('solo bebo agua💧');
+
+//como sabemos este operador es una expresion , porque todo operador en si tambien es una expresion(+, -, *. /, etc). osea que producira un valor en javascript, por lo tanto lo podemos almacenar en una variable
+const bebida = age >= 18 ? 'vino🍷' : 'agua💧';
+
+// y como toda expresion ahora la podemos utilizar en una plantilla literal, a diferencia de if y switch que son declaraciones y las plantillas esperan expresiones y no declaraciones.
+
+console.log(`me gusta beber ${bebida}`); //podemois poner toda la expresion completa tambien
+
+//veamos como seria esto en una sentencion if. RECORDAR que debemos declarar las variables fuera del bloque para que puedan ser leidas por esta sentencia esto lo veremos DESPUES
+let bebida2;
+if (age >= 18) {
+  bebida2 = 'vino';
+} else {
+  bebida2 = 'agua';
+}
+console.log(bebida);
